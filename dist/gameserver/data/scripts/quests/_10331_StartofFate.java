321994d21c76ec2efee9673667aab58a72638bba
vegax987 <vegax987@ddb26cb5-4a73-cbbe-04ab-141080f44658>
2013-03-23 15:57:26 +0000

    Removed level check for the 1st class transfer quest: Start of Fate, it caused quest freeze in Evil Incubator

--- dist/gameserver/data/scripts/quests/_10331_StartofFate.java
+++ dist/gameserver/data/scripts/quests/_10331_StartofFate.java
@@ -420,10 +420,6 @@ public class _10331_StartofFate extends Quest implements ScriptFile
 				else if ((cond >= 6) && (cond <= 11))
 				{
 					Race race = player.getRace();
-					if ((cond == 0) && (qs.getPlayer().getLevel() < 18))
-					{
-						htmltext = "Only characters under level 18 can accept this quest";
-					}
 					else if (player.isMageClass())
 					{
 						if (race == Race.human)
