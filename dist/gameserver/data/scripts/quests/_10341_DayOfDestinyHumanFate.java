7250d2d6b2098ece478f1cb2400f27642c8f883d
andreagighi@gmail.com <andreagighi@gmail.com@ddb26cb5-4a73-cbbe-04ab-141080f44658>
2013-05-07 10:48:28 +0000

    - Day Of Destiny quests fix (translating html...)

--- dist/gameserver/data/scripts/quests/_10341_DayOfDestinyHumanFate.java
+++ dist/gameserver/data/scripts/quests/_10341_DayOfDestinyHumanFate.java
@@ -3,7 +3,7 @@ package quests;
 import lineage2.gameserver.model.base.Race;
 import lineage2.gameserver.scripts.ScriptFile;
 
-public class _10346_DayOfDestinyKamaelsFate extends SagasSuperclass implements ScriptFile 
+public class _10341_DayOfDestinyHumanFate extends SagasSuperclass implements ScriptFile 
 {
 	@Override
 	public void onLoad()
@@ -20,12 +20,12 @@ public class _10346_DayOfDestinyKamaelsFate extends SagasSuperclass implements S
 	{
 	}
 
-	public _10346_DayOfDestinyKamaelsFate()
+	public _10341_DayOfDestinyHumanFate()
 	{
 		super(false);
 
-		StartNPC = 32221;
-		StartRace = Race.kamael;
+		StartNPC = 30857;
+		StartRace = Race.human;
 
 		init();
 	}
