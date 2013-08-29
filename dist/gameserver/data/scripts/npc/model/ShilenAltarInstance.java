37b41399bec95834fb6ac89a00ec7f62c5c31093
andreagighi@gmail.com <andreagighi@gmail.com@ddb26cb5-4a73-cbbe-04ab-141080f44658>
2013-08-29 07:53:47 +0000

    - Commented unused vars.

--- dist/gameserver/data/scripts/npc/model/ShilenAltarInstance.java
+++ dist/gameserver/data/scripts/npc/model/ShilenAltarInstance.java
@@ -17,17 +17,17 @@ public final class ShilenAltarInstance extends NpcInstance
 	/**
 	 * 
 	 */
-	private static final int DoorEnter1 = 25180001;
-	private static final int DoorEnter2 = 25180002;
-	private static final int DoorEnter3 = 25180003;
-	private static final int DoorEnter4 = 25180004;
-	private static final int DoorEnter5 = 25180005;
-	private static final int DoorEnter6 = 25180006;
-	private static final int DoorEnter7 = 25180007;
+	//private static final int DoorEnter1 = 25180001;
+	//private static final int DoorEnter2 = 25180002;
+	//private static final int DoorEnter3 = 25180003;
+	//private static final int DoorEnter4 = 25180004;
+	//private static final int DoorEnter5 = 25180005;
+	//private static final int DoorEnter6 = 25180006;
+	//private static final int DoorEnter7 = 25180007;
 	private static final Location FLOOR2 = new Location(179336, 13608, -9852);
 	private static final Location FLOOR3 = new Location(179304, 12824, -12796);
 	private static final Location OFF = new Location(193353, 22608, -3616);
-	private long _savedTime;
+	//private long _savedTime;
 	//DoorInstance _door1 = getReflection().getDoor(DoorEnter1);
 
 	public ShilenAltarInstance(int objectId, NpcTemplate template)
